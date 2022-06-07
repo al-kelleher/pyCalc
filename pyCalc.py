b85db2147210010fe3ae751a3b6804e5259a06d0
Alfred <al.kelleher@yahoo.com>
2022-06-07 16:18:57 -0700

    updated stuff

--- pyCalc.py
+++ pyCalc.py
@@ -1,41 +1,43 @@
 import operator
+from re import X
 
 print("\nSimple pyCalc\n")
 
+# Variable definitions
 add = 'Addition'
 sub = 'Subtraction'
 mult = 'Multiplication'
 divi = 'Division'
-arthmatic = [add, sub, mult, divi]
 
+x = int(input("Enter first digit: "))
+
+# Operation functions
 def addition():
-    z = x
     y = int(input("\nEnter second digit: "))
-    addResult = operator.add(z, y)
+    addResult = operator.add(x, y)
     print("\nYour result is: ", addResult)
 
 def subtraction():
-    z = x
     y = int(input("\nEnter second digit: "))
-    subResult = operator.sub(z, y)
+    subResult = operator.sub(x, y)
     print("\nYour result is: ",subResult)
 
 def multiplication():
-    z = x
     y = int(input("\nEnter second digit: "))
-    multResult = operator.mul(z, y)
+    multResult = operator.mul(x, y)
     print("\nYour result is: ",multResult)
 
 def division():
-    z = x
     y = int(input("\nEnter second digit: "))
-    diviResult = operator.truediv(z, y)
+    diviResult = operator.truediv(x, y)
     print("\nYour result is: ",diviResult)
 
-x = int(input("Enter first digit: "))
 print("\nWhat type of operation would you like to do?")
+
 operation = input("Response: ")
 
+
+# Operation choices
 if operation == add:
     addition()
 elif operation == sub:
